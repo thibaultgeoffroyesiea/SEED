58db5d7761c906618f67ead61a46c71a96b5a4d2
thibaultgeoffroyesiea <99199229+thibaultgeoffroyesiea@users.noreply.github.com>
2025-02-21 14:05:31 +0100

    Update main_incremental.py

--- src/main_incremental.py
+++ src/main_incremental.py
@@ -241,6 +241,7 @@ def main(argv=None):
     acc_tag = np.zeros((max_task, max_task))
     forg_taw = np.zeros((max_task, max_task))
     forg_tag = np.zeros((max_task, max_task))
+    predictions = []
     for t, (_, ncla) in enumerate(taskcla):
         # Early stop tasks if flag
         if t >= max_task:
@@ -321,8 +322,8 @@ def main(argv=None):
             logger.log_figure(name='weights', iter=t, figure=weights)
             logger.log_figure(name='bias', iter=t, figure=biases)
 
-        pred = appr.predict(tst_loader[t])
-        print("accuracy on task ", t, " : ", pred)
+        predictions.append(appr.predict(tst_loader[t]))
+    print(predictions)
     # Print Summary
     utils.print_summary(acc_taw, acc_tag, forg_taw, forg_tag)
     print('[Elapsed time = {:.1f} h]'.format((time.time() - tstart) / (60 * 60)))
