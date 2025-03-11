a6fcd0594e7e6e05a08b6eff1f78fcf1829bfb99
thibaultgeoffroyesiea <99199229+thibaultgeoffroyesiea@users.noreply.github.com>
2025-03-11 15:55:35 +0100

    Update seed.py

--- src/approach/seed.py
+++ src/approach/seed.py
@@ -401,9 +401,9 @@ class Appr(Inc_Learning_Appr):
                 mask[:, bb_num, c] = True
 
             print("***********DIM1***********")
-            print(softmax_temperature(log_probs[:, bb_num, :], dim=0, tau=self.tau).shape)
+            print(softmax_temperature(log_probs[:, bb_num, :], dim=0, tau=self.tau))
             print("***********DIM2***********")
-            print(softmax_temperature(log_probs[:, bb_num, :], dim=1, tau=self.tau).shape)
+            print(softmax_temperature(log_probs[:, bb_num, :], dim=1, tau=self.tau))
             print("***********LOGPROBS***********")
             log_probs[:, bb_num, :] = softmax_temperature(log_probs[:, bb_num, :], dim=0, tau=self.tau)
             print(log_probs)
