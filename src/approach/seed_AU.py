e8fd11ff8e0913aa8e8c364664c4b0cd244ec736
thibaultgeoffroyesiea <99199229+thibaultgeoffroyesiea@users.noreply.github.com>
2025-03-26 11:20:46 +0100

    Update seed_AU.py

--- src/approach/seed_AU.py
+++ src/approach/seed_AU.py
@@ -366,6 +366,7 @@ class Appr(Inc_Learning_Appr):
             print(aus.shape)
             print(features.shape)    
             features  = np.concatenate((features.cpu().numpy(), aus), axis=1)
+            features = features.reshape(128, 1, 81)
             features = torch.tensor(features).to(self.device)
             hits_taw, hits_tag = self.calculate_metrics(features, targets, t)
             # Log
