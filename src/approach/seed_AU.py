0edb0ea093bf24deb8ae5bd4247ceec1a1d05448
thibaultgeoffroyesiea <99199229+thibaultgeoffroyesiea@users.noreply.github.com>
2025-03-26 10:49:51 +0100

    Update seed_AU.py

--- src/approach/seed_AU.py
+++ src/approach/seed_AU.py
@@ -392,6 +392,9 @@ class Appr(Inc_Learning_Appr):
         mask = torch.full_like(log_probs, fill_value=False, dtype=torch.bool)
         for bb_num, _ in enumerate(self.experts_distributions):
             for c, class_gmm in enumerate(self.experts_distributions[bb_num]):
+                print("****CLASS_GMM****")
+                print(bb_num)
+                print(features[:, bb_num].shape)
                 c += self.model.task_offset[bb_num]
                 log_probs[:, bb_num, c] = class_gmm.score_samples(features[:, bb_num])
                 mask[:, bb_num, c] = True
