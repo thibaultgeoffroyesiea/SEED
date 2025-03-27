e198103ea7ed35bdd2f7ca2d7c9f444cfbeaa7c8
thibaultgeoffroyesiea <99199229+thibaultgeoffroyesiea@users.noreply.github.com>
2025-03-27 13:50:53 +0100

    Update main_incremental.py

--- src/main_incremental.py
+++ src/main_incremental.py
@@ -329,10 +329,12 @@ def main(argv=None):
             # pred = appr.predict(tst_loader[u])
             # pred_classes.extend(appr.predict_all_expert(tst_loader[u]))
             # predictions.extend(pred)
-            # if algo  = AU:
-            targets.extend([t[i].item() for _, _, t in tst_loader[u] for i in range(t.size(0))])
-            # else:
-            #     targets.extend([t[i].item() for _, t in tst_loader[u] for i in range(t.size(0))])
+            #check if parameter contains AU
+            print(appr_args.__dict__["approach"])
+            if 'AU' in appr_args.__dict__["approach"]:
+                targets.extend([t[i].item() for _, _, t in tst_loader[u] for i in range(t.size(0))])
+            else:
+                targets.extend([t[i].item() for _, t in tst_loader[u] for i in range(t.size(0))])
 
 
             
